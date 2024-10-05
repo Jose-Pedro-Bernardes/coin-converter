0420d15b19ae59adc2928e297e942f6f0d1dbfcd
jose-pedro <josepedro.bernardes1@gmail.com>
2024-10-05 10:45:17 -0300

    feat(main page): começando a definir estrutura do html

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+function renderHeader (){
+    const headerHtml = `
+    <header>
+        <section>
+            <figure>
+            <img src"../assets/coin.png"/>
+            </figure>
+            <p><span>€oin</span>€onverter</p>
+        </section>
+        <section>
+            <nav>
+                <button>Dólar americano</button>
+                <button>Euro</button>
+                <button>Bitcoin</button>
+                <button>Iene</button>
+                <button>Libra esterlina</button>
+            </nav>
+            <div>
+                <figure></figure>
+                <select></select>
+            </div>
+        </section>
+    </header>
+    `
+}
